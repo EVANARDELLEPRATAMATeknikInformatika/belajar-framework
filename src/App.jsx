f20898dd8d1e97251f85a6e48852bb05ebc907f3
Evan Ardelle Pratama <evan.pratama1304@gmail.com>
2026-03-09 23:12:46 +0700

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px', marginLeft: '160px' }}>
+    <div style={{ textAlign: 'center', marginTop: '50px', marginLeft: '170px' }}>
       <h1>Hello World dari ReactJS!</h1>
     </div>
   )
